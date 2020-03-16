a79b07d0f742292b0343c993c05db6be2f79bde8
Conner Foster <connerf99@gmail.com>
2020-03-15 23:00:01 -0400

    Got the sounds figured out

--- drumKit.js
+++ drumKit.js
@@ -1,2 +1,25 @@
-const clapButton = document.getElementsByClassName("button1");
-const hihatButton = document.getElementsByClassName("button2");
+const clapButton = document.getElementById("button1");
+const hihatButton = document.getElementById("button2");
+const kickButton = document.getElementById("button3");
+const openhatButton = document.getElementById("button4");
+const boomButton = document.getElementById("button5");
+const rideButton = document.getElementById("button6");
+const snareButton = document.getElementById("button7");
+const tomButton = document.getElementById("button8");
+const tinkButton = document.getElementById("button9");
+let clapAudio = new Audio();
+
+window.addEventListener("keypress", checkKeyPressed);
+
+function checkKeyPressed(e) {
+    clapAudio.src = "Sounds/clap.wav";
+if(e.key == "a" || e.key == "A") {
+    clapAudio.play();
+}
+    
+}
+
+
+
+
+
